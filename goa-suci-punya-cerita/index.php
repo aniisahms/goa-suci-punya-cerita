685c080a323e8252ebcdc7f4e5bd7032c189a332
aniisahms <83533036+aniisahms@users.noreply.github.com>
2022-06-20 18:54:30 +0700

    Add files via upload

---
+++ goa-suci-punya-cerita/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>Goa Suci Punya Cerita</title>
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Construction Html5 Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+    <meta name="author" content="Themefisher">
+    <meta name="generator" content="Themefisher Constra HTML Template v1.0">
+    
+    <!-- Animate css -->
+    <link rel="stylesheet" href="css/animate.css">
+    
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color:#a78a7f;">
+        <div class="container-fluid" style="margin-left: 30px">
+            <a class="navbar-brand" href="index.php">HOME</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mynavbar">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="stories.php">Stories</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <table>
+        <tr>
+            <td style="width: 30%;">
+                <div class="container-fluid mt-3">
+                    <p>
+                        <h1 style="color:white;">
+                        GOA <br>
+                        SUCI
+                        </h1>
+                    
+                    <h2 style="color:#a78a7f;">Punya Cerita...</h2>
+                    </p>
+
+                    <p style="color:#a78a7f; text-align: left;">
+                        <i>
+                        <br>
+                        Tempat berbagi cerita pengalaman dari santri
+                        Alkahfi Islamic Boarding School Bogor.
+                        </i>
+                    </p>
+                </div>
+            </td>
+            <td style="width: 30%;">
+                <div class="category-box">
+                    <a href="akhwat.php">
+                        <img style="height: 350px;" src="img/akhwat-ed.png" alt="" />
+                        <div class="content">
+                            <h3>Akhwat Edition</h3>
+                            <p style="text-align: left;">
+                                <i>Cerita dari para <br>
+                                akhwat...</i>
+                            </p>
+                        </div>
+                    </a>	
+                </div>
+                <div class="category-box">
+                    <a href="ikhwan.php">
+                        <img style="height: 350px;" src="img/ikhwan-ed.png" alt="" />
+                        <div class="content">
+                            <h3>Ikhwan Edition</h3>
+                            <p style="text-align: left;">
+                                <i>Cerita dari para <br>
+                                ikhwan...</i>
+                            </p> 
+                        </div>
+                    </a>	
+                </div>
+            </td>
+        </tr>
+    </table>
+    
+</body>
+</html>
---
+++ goa-suci-punya-cerita/stories.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>All Stories in Goa Suci Punya Cerita</title>
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Construction Html5 Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+    <meta name="author" content="Themefisher">
+    <meta name="generator" content="Themefisher Constra HTML Template v1.0">
+    
+    <!-- Animate css -->
+    <link rel="stylesheet" href="plugins/animate/animate.css">
+    
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color:#a78a7f;">
+        <div class="container-fluid" style="margin-left: 30px">
+            <a class="navbar-brand" href="index.php">HOME</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mynavbar">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="stories.php">Stories</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <p>
+        All stories...
+    </p>
+</body>
+</html>

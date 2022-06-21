fbf03f4a51ccb3d501d7d38aa137ca05f44c8dda
aniisahms <83533036+aniisahms@users.noreply.github.com>
2022-06-21 10:13:47 +0700

    Update akhwat.php

---
+++ goa-suci-punya-cerita/akhwat.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="css/style2.css">
+    
+    <title>Akhwat Stories</title>
+
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Construction Html5 Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+    <meta name="author" content="Themefisher">
+    <meta name="generator" content="Themefisher Constra HTML Template v1.0">
+    
+    <!-- Animate css -->
+    <link rel="stylesheet" href="css/animate.css">
+</head>
+<body>
+    
+</body>
+</html>
